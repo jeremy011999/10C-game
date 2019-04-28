ce8e9d9c6151dbd9e328906bd280f067e6eb9ebc
oliviaheiner <49533261+oliviaheiner@users.noreply.github.com>
2019-04-27 20:28:45 -0700

    Create game.h

---
+++ game.h
@@ -0,0 +1,19 @@
+#include "ball.h"
+#include "bricks.h"
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+
+#ifndef GAME_H
+#define GAME_H
+
+
+class game
+{
+public:
+    game();
+};
+
+#endif // GAME_H
