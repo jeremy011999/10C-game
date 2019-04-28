bf743f4f6ac7280caa667b2d1316e7833ec7d446
oliviaheiner <49533261+oliviaheiner@users.noreply.github.com>
2019-04-27 20:29:38 -0700

    Update ball.h

--- ball.h
+++ ball.h
@@ -1,1 +1,24 @@
+#ifndef BALL_H
+#define BALL_H
 
+#include <QWidget>
+#include <QGraphicsRectItem>
+#include <QObject>
+
+class ball:public QObject,public QGraphicsRectItem
+{
+    Q_OBJECT
+public:
+    ball();
+    void move();
+    void handle_collision_with_bricks();
+public slots:
+    void update_ball();
+private:
+    int x_velocity;
+    int y_velocity;
+    int ball_width;
+    int ball_height;
+};
+
+#endif // BALL_H
