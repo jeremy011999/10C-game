5e42eef235d9bf7b3a88893e98afd6cd3fd2ef3c
oliviaheiner <49533261+oliviaheiner@users.noreply.github.com>
2019-05-06 12:37:55 -0700

    Update bricks.h

--- bricks.h
+++ bricks.h
@@ -4,16 +4,18 @@
 #include <QWidget>
 #include <QGraphicsRectItem>
 #include <QObject>
+#include <QPointF>
 
 class brick:public QObject,public QGraphicsRectItem
 {
     Q_OBJECT
 public:
-    brick(int bwidth, int bheight);
+    brick(int bwidth, int bheight, int bricklevel);
+    void update_hit_brick();
 private:
     int brick_width;
     int brick_height;
+    int brick_level;
 };
 
 #endif // BRICKS_H
-
