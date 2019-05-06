7215077ac7e82a803bf696c69a60b9d41ed393d3
oliviaheiner <49533261+oliviaheiner@users.noreply.github.com>
2019-05-06 12:38:38 -0700

    Update game.h

--- game.h
+++ game.h
@@ -1,19 +1,29 @@
 #include "ball.h"
 #include "bricks.h"
+#include "paddle.h"
+#include "welcome_window.h"
 #include <QApplication>
 #include <QHBoxLayout>
+#include <QLayout>
 #include <QWidget>
 #include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QObject>
 
 #ifndef GAME_H
 #define GAME_H
 
 
-class game
+class game : public QObject
 {
+    Q_OBJECT
 public:
     game();
+public slots:
+    void run_game();
+private:
+    welcome_window* w = nullptr;
+    int points=0;
 };
 
 #endif // GAME_H
