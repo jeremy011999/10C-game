c4864368758e9ee2daf83d9fb34ff4289d816f9a
delgebaly <38047419+delgebaly@users.noreply.github.com>
2019-06-04 14:51:06 -0700

    Update levelwonwindow.h

--- levelwonwindow.h
+++ levelwonwindow.h
@@ -13,12 +13,35 @@ class LevelWonWindow : public QWidget
     Q_OBJECT
 public:
     LevelWonWindow();
+    
+    /* set_level_just_beat(int lvl_just_beat) is responsible for text that notifies you what level you just beat
+    @param int lvl_just_beat - value of the level just beat
+    @return void
+    */
     void set_level_just_beat(int lvl_just_beat);
 signals:
+    /* returnToMainWindow() is a signal emitted to return to the main screen
+    @param none
+    @return void
+    */
     void returnToMainWindow();
+    
+     /* goToLevel(int lvl) is a signal emitted to go to the next level
+    @param int lvl - level number of next level
+    @return none
+    */
     void goToLevel(int lvl);
 public slots:
+    /* nextLevelButtonClicked() is a slot responsible for managing the clicking of the next level button
+    @param
+    @return
+    */
     void nextLevelButtonClicked();
+    
+    /* quitButtonClicked() is a slot responsible for managing the clicking of the quit button
+    @param none
+    @return void
+    */
     void quitButtonClicked();
 private:
     QVBoxLayout* mainLayout;
