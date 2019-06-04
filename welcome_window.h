e1bde8eea02c12af133c6bf7eebef942f11f8414
delgebaly <38047419+delgebaly@users.noreply.github.com>
2019-06-04 13:53:50 -0700

    Update welcome_window.h

--- welcome_window.h
+++ welcome_window.h
@@ -19,17 +19,64 @@ class welcome_window : public QMainWindow
 public:
     explicit welcome_window(QWidget *parent = nullptr);
     ~welcome_window();
+    /* paintEvent(QPaintEvent *e) sets background image of welcome window
+    @param QPaintEvent *e contains object pixmap is called on
+    @return void
+    */
     void paintEvent(QPaintEvent *e);
 public slots:
+    /* GameButtonClicked() slot for clicking start game
+    @param none
+    @return void
+    */
     void GameButtonClicked();
+    
+    /* quitButtonclicked() slot for clicking quit button
+    @param none 
+    @return void
+    */
     void quitButtonClicked();
+    
+    /* instructionsButtonClicked() slot for clicking instructions window button
+    @param none
+    @return void
+    */
     void instructionsButtonClicked();
+    
+    /* OptionsButtonClicked() slot for clicking options window button
+    @param none
+    @return void
+    */
     void OptionButtonClicked();
+    
+    /* resizeWindow(int sizefactor) slot for adjusting window size
+    @param int sizefactor - this changes size of window by factor
+    @return void
+    */
     void resizeWindow(int sizefactor);
 signals:
+    /* signal to start game
+    @param none
+    @return void
+    */
     void startGame();
+    
+    /* signal to quit game
+    @param none
+    @return void
+    */
     void quitApplication();
+    
+    /* signal to show instructions
+    @param none
+    @return void
+    */
     void showInstructions();
+    
+    /* signal to show options
+    @param none
+    @return return void
+    */
     void showOptionsWindow();
 private:
     QLabel* welcomePage = nullptr;
