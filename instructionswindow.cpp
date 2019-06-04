f02a2d7d790386f820eeaaadc130cb82b8877383
jeremy011999 <49453574+jeremy011999@users.noreply.github.com>
2019-06-04 12:55:13 -0700

    Update instructionswindow.cpp

--- instructionswindow.cpp
+++ instructionswindow.cpp
@@ -1,6 +1,12 @@
 #include "instructionswindow.h"
 #include "ui_instructionswindow.h"
 
+
+/*
+Constructor for InstructionsWindow object
+    sets up UI and stackedwidget for each page
+@param parent: sets parent to QMainWindow
+*/
 InstructionsWindow::InstructionsWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::InstructionsWindow)
@@ -11,36 +17,62 @@ InstructionsWindow::InstructionsWindow(QWidget *parent) :
     ui->centralWidget->setStyleSheet("QStackedWidget { background-color: rgb(255, 255, 255); color: rgb(255, 255, 255);}");
 }
 
+/*
+destructor for instructionswindow that
+cleans up UI
+*/
 InstructionsWindow::~InstructionsWindow()
 {
     delete ui;
 }
 
+/*
+Slot that sets the current stacked widget to the enemy instructions page
+(works for next button)
+*/
 void InstructionsWindow::on_NextPageEnemy_clicked()
 {
     ui->stackedWidget->setCurrentIndex(1);
 }
 
+/*
+Slot that sets the current stacked widget to the powerup instructions page
+(works for next button)
+*/
 void InstructionsWindow::on_NextPagePowerups_clicked()
 {
     ui->stackedWidget->setCurrentIndex(2);
 }
 
+/*
+Slot that emits signal to let gui know to go back to main menu
+*/
 void InstructionsWindow::on_ReturnToMainMenu_clicked()
 {
     emit returnButtonClicked();
 }
 
+/*
+Slot that sets current stacked widget to first instructions page
+(works for back button)
+*/
 void InstructionsWindow::on_PrevPageHowToPlay_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
 }
 
+/*
+Slot that sets the current stacked widget to the enemy instructions page
+(works on back button)
+*/
 void InstructionsWindow::on_PrevPageEnemies_clicked()
 {
     ui->stackedWidget->setCurrentIndex(1);
 }
 
+/*
+Slot that emits signal to let gui know to go back to main menu
+*/
 void InstructionsWindow::on_MainMenuButton_clicked()
 {
     emit returnButtonClicked();
