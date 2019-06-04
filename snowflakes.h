efb89c847faeb4f3e7653764b0f91159d329d42b
delgebaly <38047419+delgebaly@users.noreply.github.com>
2019-06-04 14:45:14 -0700

    Update snowflakes.h

--- snowflakes.h
+++ snowflakes.h
@@ -19,13 +19,36 @@ class snowflake : public QObject, public QGraphicsPixmapItem
     Q_OBJECT
 public:
     snowflake(double size);
+    
+    /* move() moves the snowflake according to x and y velocity
+    @param none
+    @return void
+    */ 
     void move();
     virtual ~snowflake();
+    
+    /* stop_timer() stops the timer when function is called
+    @param none
+    @return void
+    */
     void stop_timer();
 public slots:
+    /* updateSnowFlake() is a slot that checks for collisions with the paddle and allows necessary signals and slots to update snowflake
+    @param none
+    @return void
+    */
     virtual void updateSnowflake();
+    
+    /* snowflakeHit() is a slot that stops the timer and ermoves the snowflake from the scene when hit
+    @param
+    @return
+    */
     void snowflakeHit();
 signals:
+    /* snowflake_captured() is a signal emitted to capture the snowflake
+    @param none
+    @return void
+    */
     void snowflake_captured();
 protected:
     double y_velocity,x_velocity;
