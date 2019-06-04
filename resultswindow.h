bdd143b6224dfab5d32f36610de99309fef72555
delgebaly <38047419+delgebaly@users.noreply.github.com>
2019-06-04 14:05:32 -0700

    Update resultswindow.h

--- resultswindow.h
+++ resultswindow.h
@@ -11,10 +11,23 @@ class resultswindow : public QWidget
     Q_OBJECT
 public:
     resultswindow();
+    
+    /* set_points_label(int pnts) is a function that adjusts the label to include the value of your final score
+    @param int pnts - the number of points you accumulate during the game before you lose
+    @return none
+    */
     void set_points_label(int pnts);
 public slots:
+    /* quitResultsButtonClicked() slot to click return to main screen
+    @param none
+    @return void
+    */
     void quitResultsButtonClicked();
 signals:
+    /* quitButtonClicked() signal to click quit game Button
+    @param none
+    @return void
+    */
     void quitButtonClicked();
 private:
     QPushButton* quitResultsButton = nullptr;
