3630f4da1cadd64ce84c48d7299f3e2b6ae983e5
delgebaly <38047419+delgebaly@users.noreply.github.com>
2019-06-04 13:29:33 -0700

    Update greensnowflake.h

--- greensnowflake.h
+++ greensnowflake.h
@@ -10,9 +10,18 @@ class greenSnowflake:public snowflake
     Q_OBJECT
 public:
     greenSnowflake(double size);
+    
+    /* updateSnowflake() checks for collisions and emits signal to capture snowflake, effectively adding plus life image
+    @param no parameters
+    @return void, doesn't return anything
+    */
     void updateSnowflake();
     ~greenSnowflake();
 signals:
+    /* green_snowflake_captured() is a signal emitted by updateSnowFlake() function which checks whether paddle has captured snowflake
+    @param no parameters
+    @return void, doesn't return anything
+    */
     void green_snowflake_captured();
 private:
     QPixmap* plusLifePic = nullptr;
