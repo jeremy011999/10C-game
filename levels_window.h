6587cec39c47acbc5117b716aae86b8477b5588a
jeremy011999 <49453574+jeremy011999@users.noreply.github.com>
2019-06-04 18:57:35 -0700

    Update levels_window.h

--- levels_window.h
+++ levels_window.h
@@ -15,18 +15,42 @@
 #include <QLabel>
 #include "event_filters.h"
 
-class level_pic;
+class level_pic; //forward declaration of level_pic
 
 class levels_window : public QMainWindow
 {
     Q_OBJECT
 public:
+
+    /*
+    Constructor for levels_window, sets up buttons and pixmap for level preview
+    @param parent: sets parent of levels_window (defaulted to nullptr)
+    */
     explicit levels_window(QWidget *parent = nullptr);
+    
+    /*
+    handles mouse hover events to preview picture of level that is being hovered over
+    @param e: refers to any hover event that is currently being performed on level select buttons
+    */
     void hoverEvent(QHoverEvent* e);
+    
 signals:
+
+    //lets gui know to go back to main welcome window
     void returnToMainWindow();
+    
+    /*
+    lets game know to start and what level to start at
+    @param lvl: refers to level number that game will start at (1-5)
+    */
     void goToLevel(int lvl);
+    
 public slots:
+
+    /*
+    resizes window to either large or small icons
+    @param sizeFactor: ranges from 1-2 and lets window know whether icons should be small(1) or large(2)
+    */
     void resizeWindow(int sizeFactor);
 private:
     QVBoxLayout* mainLayout = nullptr;
@@ -51,7 +75,11 @@ class level_pic : public QLabel
 {
     Q_OBJECT
 public:
+    
+    //Constructor for level pic object that holds the current preview image of level
     level_pic();
+    
+    //Destructor for level pic object to properly clean up pixmap objects
     ~level_pic()
     {
         delete blanklvlpic;
@@ -67,14 +95,26 @@ public:
         frthlvlpic     = nullptr;
         fthlvlpic      = nullptr;
     }
-signals:
 public slots:
+
+    //sets current picture to picture of first level
     void pic1();
+    
+    //sets current picture to picture of second level
     void pic2();
+    
+    //sets current picture to picture of third level
     void pic3();
+    
+    //sets current picture to picture of fourth level
     void pic4();
+    
+    //sets current picture to picture of fifth level
     void pic5();
+    
+    //sets current picture to picture of blank image
     void show_blank();
+    
 private:
     QPixmap* blanklvlpic = new QPixmap(":/Game_Media/Pictures/blank.png");
     QPixmap* firstlvlpic = new QPixmap(":/Game_Media/Pictures/first.png");
