424b8c0459237964919dd5d14657c594349e4e02
jeremy011999 <49453574+jeremy011999@users.noreply.github.com>
2019-06-04 19:33:48 -0700

    Update powerupmeter.h

--- powerupmeter.h
+++ powerupmeter.h
@@ -10,14 +10,45 @@ class powerUpMeter : public QWidget
     Q_OBJECT
 
 public:
+
+    /*
+    Constructor for powerUpMeter object
+    @param parent: sets parent of object
+    */
     powerUpMeter(QWidget *parent = 0);
+    
+    //Destrcutor for powerUpMeter that properly cleans up pixmaps
     ~powerUpMeter();
+    
+    /*
+    changes current image for powerUpMeter using counter variable
+    @param e: refers to paint event that is currently happening to powerUpMeter object
+    */
     void paintEvent(QPaintEvent *e);
+    
+    /*
+    lets powerUpMeter know if its currently showing full picture
+    @return: return true if full, false if not
+    */
     bool is_full();
+    
+    /*
+    lets powerUpMeter know if its currently showing empty picture
+    @return: return true if empty, false if not
+    */
     bool is_empty();
+    
 public slots:
+
+    //increases counter when powerup is earned and then repaints to appropriate image
     void update_counter();
+    
+    /*
+    sets the current counter to a specific value
+    @param num: is number to which counter is set to
+    */
     void set_counter(int num);
+    
 private:
     int counter = 0;
     QPixmap* emptySnowflake = new QPixmap(":/Game_Media/Pictures/imageedit_7_4838096674.png");
