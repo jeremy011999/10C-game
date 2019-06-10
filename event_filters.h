36963bd6b10de10dc03b466828d659971869f20e
oliviaheiner <49533261+oliviaheiner@users.noreply.github.com>
2019-06-10 10:38:18 -0700

    Update event_filters.h

--- event_filters.h
+++ event_filters.h
@@ -18,21 +18,23 @@ class event_filter1 : public QObject
     Q_OBJECT
 public:
 
-    /*
-    Constructors for event_filters 1 (each filter corresponds to a button on
-        levelswindow page)
-    @param parent: sets parents to QObject (inherits from QObject)
-    */
+    /**
+     * Constructors for event_filters 1 (each filter corresponds to a button on
+     * levelswindow page)
+     *
+     * @param parent: sets parents to QObject (inherits from QObject)
+     */
     explicit event_filter1(QObject *parent = nullptr);
     
-    /*
-    sets up event filter to take care of hover_enter and hover_leave events
-        in order to display level picture when mouse moves over a button
-        as well as change stylesheet of button
-    @param: obj: lets the event filter know what it is being attached to
-    @param event: refers to event that is currently happening to obj
-    @return: returns whether or not anything should be done for event (true for yes)
-    */
+    /**
+     * sets up event filter to take care of hover_enter and hover_leave events
+     * in order to display level picture when mouse moves over a button
+     * as well as change stylesheet of button
+     *
+     * @param: obj: lets the event filter know what it is being attached to
+     * @param event: refers to event that is currently happening to obj
+     * @return: returns whether or not anything should be done for event (true for yes)
+     */
     bool eventFilter(QObject* obj, QEvent* event) override;
     
 signals:
@@ -49,21 +51,23 @@ class event_filter2 : public QObject
     Q_OBJECT
 public:
 
-    /*
-    Constructors for event_filters 2 (each filter corresponds to a button on
-        levelswindow page)
-    @param parent: sets parents to QObject (inherits from QObject)
-    */
+    /**
+     * Constructors for event_filters 2 (each filter corresponds to a button on
+     * levelswindow page)
+     *
+     * @param parent: sets parents to QObject (inherits from QObject)
+     */
     explicit event_filter2(QObject *parent = nullptr);
     
-    /*
-    sets up event filter to take care of hover_enter and hover_leave events
-        in order to display level picture when mouse moves over a button
-        as well as change stylesheet of button
-    @param: obj: lets the event filter know what it is being attached to
-    @param event: refers to event that is currently happening to obj
-    @return: returns whether or not anything should be done for event (true for yes)
-    */
+    /**
+     * sets up event filter to take care of hover_enter and hover_leave events
+     * in order to display level picture when mouse moves over a button
+     * as well as change stylesheet of button
+     *
+     * @param: obj: lets the event filter know what it is being attached to
+     * @param event: refers to event that is currently happening to obj
+     * @return: returns whether or not anything should be done for event (true for yes)
+     */
     bool eventFilter(QObject* obj, QEvent* event) override;
     
 signals:
@@ -80,21 +84,23 @@ class event_filter3 : public QObject
     Q_OBJECT
 public:
 
-    /*
-    Constructors for event_filters 3 (each filter corresponds to a button on
-        levelswindow page)
-    @param parent: sets parents to QObject (inherits from QObject)
-    */
+    /**
+     * Constructors for event_filters 3 (each filter corresponds to a button on
+     * levelswindow page)
+     *
+     * @param parent: sets parents to QObject (inherits from QObject)
+     */
     explicit event_filter3(QObject *parent = nullptr);
     
-    /*
-    sets up event filter to take care of hover_enter and hover_leave events
-        in order to display level picture when mouse moves over a button
-        as well as change stylesheet of button
-    @param: obj: lets the event filter know what it is being attached to
-    @param event: refers to event that is currently happening to obj
-    @return: returns whether or not anything should be done for event (true for yes)
-    */
+    /**
+     * sets up event filter to take care of hover_enter and hover_leave events
+     * in order to display level picture when mouse moves over a button
+     * as well as change stylesheet of button
+     *
+     * @param: obj: lets the event filter know what it is being attached to
+     * @param event: refers to event that is currently happening to obj
+     * @return: returns whether or not anything should be done for event (true for yes)
+     */
     bool eventFilter(QObject* obj, QEvent* event) override;
     
 signals:
@@ -111,21 +117,23 @@ class event_filter4 : public QObject
     Q_OBJECT
 public:
 
-    /*
-    Constructors for event_filters 4 (each filter corresponds to a button on
-        levelswindow page)
-    @param parent: sets parents to QObject (inherits from QObject)
-    */
+    /**
+     * Constructors for event_filters 4 (each filter corresponds to a button on
+     * levelswindow page)
+     *
+     * @param parent: sets parents to QObject (inherits from QObject)
+     */
     explicit event_filter4(QObject *parent = nullptr);
     
-    /*
-    sets up event filter to take care of hover_enter and hover_leave events
-        in order to display level picture when mouse moves over a button
-        as well as change stylesheet of button
-    @param: obj: lets the event filter know what it is being attached to
-    @param event: refers to event that is currently happening to obj
-    @return: returns whether or not anything should be done for event (true for yes)
-    */
+    /**
+     * sets up event filter to take care of hover_enter and hover_leave events
+     * in order to display level picture when mouse moves over a button
+     * as well as change stylesheet of button
+     *
+     * @param: obj: lets the event filter know what it is being attached to
+     * @param event: refers to event that is currently happening to obj
+     * @return: returns whether or not anything should be done for event (true for yes)
+     */
     bool eventFilter(QObject* obj, QEvent* event) override;
     
 signals:
@@ -142,21 +150,23 @@ class event_filter5 : public QObject
     Q_OBJECT
 public:
 
-    /*
-    Constructors for event_filters 5 (each filter corresponds to a button on
-        levelswindow page)
-    @param parent: sets parents to QObject (inherits from QObject)
-    */
+    /**
+     * Constructors for event_filters 5 (each filter corresponds to a button on
+     * levelswindow page)
+     *
+     * @param parent: sets parents to QObject (inherits from QObject)
+     */
     explicit event_filter5(QObject *parent = nullptr);
     
-    /*
-    sets up event filter to take care of hover_enter and hover_leave events
-        in order to display level picture when mouse moves over a button
-        as well as change stylesheet of button
-    @param: obj: lets the event filter know what it is being attached to
-    @param event: refers to event that is currently happening to obj
-    @return: returns whether or not anything should be done for event (true for yes)
-    */
+    /**
+     * sets up event filter to take care of hover_enter and hover_leave events
+     * in order to display level picture when mouse moves over a button
+     * as well as change stylesheet of button
+     *
+     * @param: obj: lets the event filter know what it is being attached to
+     * @param event: refers to event that is currently happening to obj
+     * @return: returns whether or not anything should be done for event (true for yes)
+     */
     bool eventFilter(QObject* obj, QEvent* event) override;
     
 signals:
